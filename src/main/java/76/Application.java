7bfe167ed15e36016faa6496ea383c721f332580
root <root@ip-172-31-22-123.ap-southeast-2.compute.internal>
2024-01-31 05:12:30 +0000

    Project3

---
+++ src/main/java/76/Application.java
@@ -0,0 +1,13 @@
+package 76;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication(scanBasePackages = {"pl.beone.promena"})
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
